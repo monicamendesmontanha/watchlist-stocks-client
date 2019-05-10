b6a1605b9d0b4c001277041a40015b8c9f5427e5
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-10 14:34:43 +1000

    Add symbol, companyName, Price and changePercent

--- src/components/mainPage/WatchList.js
+++ src/components/mainPage/WatchList.js
@@ -1,10 +1,65 @@
 import React, { Component }  from 'react';
+import axios from "axios";
+
+const SERVER_URL = "https://api.iextrading.com/1.0/stock/aapl/quote";
 
 class WatchList extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      symbol: '',
+      companyName: '',
+      price: '',
+      oscilation: ''
+    };
+
+    const fetchSymbol = () => {
+      axios.get(SERVER_URL).then( (results) => {
+        this.setState({ symbol: results.data.symbol });
+      });
+    };
+    fetchSymbol();
+
+    const fetchCompanyName = () => {
+      axios.get(SERVER_URL).then( (results) => {
+        this.setState({ companyName: results.data.companyName});
+      });
+    };
+    fetchCompanyName();
+
+    const fetchPrice = () => {
+      axios.get(SERVER_URL).then( (results) => {
+        // console.log(results)
+        this.setState({ price: results.data.calculationPrice});
+      });
+    };
+    fetchPrice();
+
+    const fetchChangePercent = () => {
+      axios.get(SERVER_URL).then( (results) => {
+        this.setState({ changePercent: results.data.changePercent});
+      });
+    };
+    fetchChangePercent();
+
+  }
+
   render() {
     return(
       <div>
-        watchlist coming soon
+        <h1>STOCKS</h1>
+
+        <div>
+          <div>{ this.state.symbol}</div>
+          <div>{ this.state.companyName }</div>
+        </div>
+
+        <div>
+          <div>{ this.state.price }</div>
+          <div>{ this.state.changePercent}</div>
+        </div>
+
       </div>
     );
   }
