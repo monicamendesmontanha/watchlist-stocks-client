669b51e5d6b3eb60fc86d20cbecbdee2da4a8448
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-10 22:58:09 +1000

    Change links order

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import WatchList from './mainPage/WatchList'
-import Menu from './header/Menu'
+import Menu from './header/Menu';
+import WatchList from './mainPage/WatchList';
 
 function App() {
   return (
