9ec40bee05a3ab8c3f332507fec972edc5c31c5b
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-10 22:58:56 +1000

    Add StockDetails and passed data to show info page

--- src/components/mainPage/WatchList.js
+++ src/components/mainPage/WatchList.js
@@ -1,33 +1,57 @@
 import React, { Component }  from 'react';
 import axios from "axios";
 import './WatchList.css'
-import SearchStock from './SearchStock';
+import StockDetails from '../infoPage/StockDetails';
+// import SearchStock from './SearchStock';
 
-const serverUrl = (stockSymbol) => `https://api.iextrading.com/1.0/stock/${stockSymbol}/quote`;
+const serverPriceUrl = (stockSymbol) => `https://api.iextrading.com/1.0/stock/${stockSymbol}/price`;
+const serverQuoteUrl = (stockSymbol) => `https://api.iextrading.com/1.0/stock/${stockSymbol}/quote?displayPercent=true`;
+const serverStatsUrl = (stockSymbol) => `https://api.iextrading.com/1.0/stock/${stockSymbol}/stats`;
 
 class WatchList extends Component {
 
   constructor() {
     super();
     this.state = {
-      symbol: '',
-      companyName: '',
-      price: '',
-      changePercent: 0
+      selectedStock: {
+        changePercent: 0
+      }
     };
+  }
 
-    const fetchInfo = () => {
-      axios.get(serverUrl('googl')).then( (results) => {
-        this.setState({
-          symbol: results.data.symbol,
-          companyName: results.data.companyName,
-          price: results.data.calculationPrice === "close" ? results.data.close : results.data.calculationPrice,
-          changePercent: results.data.changePercent
-        });
-      });
+  async fetchInfo() {
+    const price = await axios.get(serverPriceUrl('googl'));
+    const quote = await axios.get(serverQuoteUrl('googl'));
+    const stats = await axios.get(serverStatsUrl('googl'));
+
+    console.log(quote)
+
+    const stock = {
+      price: price.data,
+
+      symbol: quote.data.symbol,
+      companyName: quote.data.companyName,
+      changePercent: quote.data.changePercent,
+
+      marketcap: stats.data.marketcap,
+      revenuePerShare: stats.data.revenuePerShare,
+      revenue: stats.data.revenue,
+      week52high: stats.data.week52high,
+      week52low: stats.data.week52low,
+      ebitda: stats.data.EBITDA,
+      dividendYield: stats.data.dividendYield,
+      profitMargin: stats.data.profitMargin,
+      returnOnEquity: stats.data.returnOnEquity,
+      ttmEPS: stats.data.ttmEPS
     };
-    fetchInfo();
+    console.log(stock)
 
+
+    this.setState({ selectedStock: stock });
+  }
+
+  componentDidMount() {
+    this.fetchInfo();
   }
 
   // TODO:
@@ -38,20 +62,21 @@ class WatchList extends Component {
   render() {
     return(
       <>
-      <SearchStock />
+      {/* <SearchStock /> */}
       <div className="watch-list">
 
         <div className="symbol_name">
-          <div className="symbol" >{ this.state.symbol }</div>
-          <div className="name" >{ this.state.companyName }</div>
+          <div className="symbol" >{ this.state.selectedStock.symbol }</div>
+          <div className="name" >{ this.state.selectedStock.companyName }</div>
         </div>
 
         <div className="price_percent">
-          <div className="price" >{ this.state.price }</div>
-          <div className="percent" >{ this.state.changePercent.toFixed(2)  }</div>
+          <div className="price" >{ this.state.selectedStock.price }</div>
+          <div className="percent" >{ this.state.selectedStock.changePercent.toFixed(2) }</div>
         </div>
-
       </div>
+
+        <StockDetails stock={this.state.selectedStock} />
       </>
     );
   }
