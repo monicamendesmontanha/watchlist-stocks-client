8fa3e6e64269ceff4057b5ed92291aec17af4806
Luke Firth <lukeantonfirth@gmail.com>
2019-05-11 01:21:01 +1000

    searchbar display test

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Menu from './header/Menu';
 import WatchList from './mainPage/WatchList';
-
+import SearchStock from './mainPage/SearchStock'
 function App() {
   return (
     <div className="App">
       <Menu />
+      <SearchStock />
       <WatchList />
     </div>
   );
