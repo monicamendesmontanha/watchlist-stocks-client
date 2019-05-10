98b8bc2a984fd5f764fa50b2235758d337a4184f
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-10 11:02:54 +1000

    Add the components structure

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import WatchList from './mainPage/WatchList'
+
+function App() {
+  return (
+    <div className="App">
+      <WatchList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/mainPage/WatchList.js
@@ -0,0 +1,13 @@
+import React, { Component }  from 'react';
+
+class WatchList extends Component {
+  render() {
+    return(
+      <div>
+        watchlist coming soon
+      </div>
+    );
+  }
+}
+
+export default WatchList;
