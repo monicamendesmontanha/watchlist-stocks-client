a66458faaa81d2b6a041e94628492f89976e1212
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-12 18:49:42 +1000

    Refactor StockDetails

--- src/components/infoPage/StockDetails.js
+++ src/components/infoPage/StockDetails.js
@@ -7,24 +7,19 @@ import './StockDetails.css'
 //   number.toFixed(2)
 // )
 
-class StockDetails extends Component {
-
-  render() {
-    return(
-      <div>
-        <p>week52Low: { this.props.stock.week52low }</p>
-        <p>week52High: { this.props.stock.week52high }</p>
-        <p>Market cap: { this.props.stock.marketcap }</p>
-        <p>Revenue: { this.props.stock.revenue }</p>
-        <p>Revenue per share: { this.props.stock.revenuePerShare }</p>
-        <p>EBITDA: { this.props.stock.ebitda }</p>
-        <p>Profit Margin: { this.props.stock.profitMargin }</p>
-        <p>ROE: { this.props.stock.returnOnEquity }</p>
-        <p>EPS: { this.props.stock.ttmEPS }</p>
-        <p>Div.Yield: { this.props.stock.dividendYield }</p>
-      </div>
-    );
-  };
+const StockDetails = (props) => {
+  return (<div>
+    <p>week52Low: {props.stock.week52low}</p>
+    <p>week52High: {props.stock.week52high}</p>
+    <p>Market cap: {props.stock.marketcap}</p>
+    <p>Revenue: {props.stock.revenue}</p>
+    <p>Revenue per share: {props.stock.revenuePerShare}</p>
+    <p>EBITDA: {props.stock.ebitda}</p>
+    <p>Profit Margin: {props.stock.profitMargin}</p>
+    <p>ROE: {props.stock.returnOnEquity}</p>
+    <p>EPS: {props.stock.ttmEPS}</p>
+    <p>Div.Yield: {props.stock.dividendYield}</p>
+  </div>);
 }
 
 export default StockDetails;
