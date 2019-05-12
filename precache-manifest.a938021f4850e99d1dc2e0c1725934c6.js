e64536439c06f50af8a259641e35b73358ef05ee
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-13 09:55:35 +1000

    Updates

---
+++ precache-manifest.a938021f4850e99d1dc2e0c1725934c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acaa7139037498a770106cfeabad7964",
+    "url": "/watchlist-stocks-client/index.html"
+  },
+  {
+    "revision": "3d63df1b512a22aa5fe4",
+    "url": "/watchlist-stocks-client/static/css/main.d8ec593e.chunk.css"
+  },
+  {
+    "revision": "884f34ba251a78290348",
+    "url": "/watchlist-stocks-client/static/js/2.e3d578ab.chunk.js"
+  },
+  {
+    "revision": "3d63df1b512a22aa5fe4",
+    "url": "/watchlist-stocks-client/static/js/main.37818f61.chunk.js"
+  },
+  {
+    "revision": "4cb537df0091d8815b48",
+    "url": "/watchlist-stocks-client/static/js/runtime~main.16778ea8.js"
+  },
+  {
+    "revision": "306a8e731bd94b67602b66c4a4fcc32b",
+    "url": "/watchlist-stocks-client/static/media/search.306a8e73.svg"
+  }
+]);
