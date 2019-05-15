2aff407a771653b57467f8877c4c87456ae1c0b5
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-15 15:35:34 +1000

    Transform the number to string to show K, M, B, T

--- src/components/infoPage/StockDetails.js
+++ src/components/infoPage/StockDetails.js
@@ -7,6 +7,17 @@ import "./StockDetails.scss";
 //   number.toFixed(2)
 // )
 
+
+function intToString (value) {
+  var suffixes = ["", "K", "M", "B","T"];
+  var suffixNum = Math.floor((""+value).length/3);
+  var shortValue = parseFloat((suffixNum != 0 ? (value / Math.pow(1000,suffixNum)) : value).toPrecision(2));
+  if (shortValue % 1 != 0) {
+      var shortNum = shortValue.toFixed(1);
+  }
+  return shortValue+suffixes[suffixNum];
+}
+
 const StockDetails = ({ stock }) => {
   return (
     <>
@@ -22,16 +33,16 @@ const StockDetails = ({ stock }) => {
       </div>
 
       <div className="stock-details">
-        <p>week52Low: $ {stock.week52low}</p>
-        <p>week52High: $ {stock.week52high}</p>
-        <p>Market cap: $ {stock.marketcap}</p>
-        <p>Revenue: $ {stock.revenue}</p>
-        <p>Revenue per share: $ {stock.revenuePerShare}</p>
-        <p>EBITDA: $ {stock.ebitda}</p>
+        <p>week52Low:($) {stock.week52low}</p>
+        <p>week52High:($) {stock.week52high}</p>
+        <p>Market cap:($) {intToString(stock.marketcap)}</p>
+        <p>Revenue:($) {stock.revenue}</p>
+        <p>Revenue per share:($) {stock.revenuePerShare}</p>
+        <p>EBITDA: {stock.ebitda}</p>
         <p>Profit Margin: $ {stock.profitMargin}</p>
-        <p>ROE: $ {stock.returnOnEquity}</p>
-        <p>EPS: $ {stock.ttmEPS}</p>
-        <p>Div.Yield: $ {stock.dividendYield}</p>
+        <p>ROE: {stock.returnOnEquity}</p>
+        <p>EPS: {stock.ttmEPS}</p>
+        <p>Div.Yield: {stock.dividendYield}</p>
       </div>
     </>
   );
